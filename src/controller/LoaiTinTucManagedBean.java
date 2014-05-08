1ac016276e5f464e3f9db28aaaa91fb7e1c35b1d
uit11520232@gmail.com <uit11520232@gmail.com@05aaaf4f-693c-68e0-440e-df912df79725>
2014-05-08 16:58:26 +0000

    news

--- src/controller/LoaiTinTucManagedBean.java
+++ src/controller/LoaiTinTucManagedBean.java
@@ -1,5 +1,44 @@
 package controller;
 
-public class LoaiTinTucManagedBean {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
+import model.LoaiTinTuc;
+import dao.LoaiTinTucDao;
 
+@ManagedBean(name= "loaiTTMBean")
+@ViewScoped
+public class LoaiTinTucManagedBean implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private  LoaiTinTuc loaiTT=new LoaiTinTuc();
+	private LoaiTinTucDao loaiTTDao=new LoaiTinTucDao();
+	private List<SelectItem> selectOneItemLTT;
+	
+	public List<SelectItem> getSelectOneItemLTT() {
+		this.selectOneItemLTT=new ArrayList<SelectItem>();
+		List<LoaiTinTuc> loaiTTs=loaiTTDao.selectItems();
+		for(LoaiTinTuc ltt:loaiTTs){
+			SelectItem selectItem=new SelectItem(ltt.getMaLTT(),ltt.getTenLTT());
+			this.selectOneItemLTT.add(selectItem);
+		}
+		return selectOneItemLTT;
+	}
+	public LoaiTinTuc getLoaiTT() {
+		return loaiTT;
+	}
+	public void setLoaiTT(LoaiTinTuc loaiTT) {
+		this.loaiTT = loaiTT;
+	}
+	public LoaiTinTucDao getLoaiTTDao() {
+		return loaiTTDao;
+	}
+	public void setLoaiTTDao(LoaiTinTucDao loaiTTDao) {
+		this.loaiTTDao = loaiTTDao;
+	}
 }
